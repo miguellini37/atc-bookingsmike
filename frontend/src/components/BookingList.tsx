8b9f094439fadaceba3b00acc21977903baf432e
mike <miguellini37@gmail.com>
2026-02-04 19:04:33 -0700

    Remove unused React import from BookingList

--- frontend/src/components/BookingList.tsx
+++ frontend/src/components/BookingList.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { format } from 'date-fns';
 import { MoreHorizontal, Pencil, Trash2, Radio, Calendar, User } from 'lucide-react';
 import { Button } from '@/components/ui/button';
